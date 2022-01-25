28e4503f6ad287a68184c6bc8b62b03822d76131
sheddyranking <shedyisaac@gmail.com>
2022-01-25 02:49:21 +0100

    made the blue(enemy) balls to fall.

--- pygame_lesson1.py
+++ pygame_lesson1.py
@@ -24,6 +24,8 @@ screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
 game_over = False
 
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,8 +45,16 @@ while not game_over:
 
     screen.fill(BACKGROUND_COLOR) #its fills the sides black
 
+    #making the blue rect fall.
+    if enemy_pos[1] >= 0 and enemy_pos[1] <= HEIGTH:
+        enemy_pos[1] +=10 
+    else:
+        enemy_pos[0] = random.randint(0, WIDTH-enemy_size) #randomizing the falling pos.
+        enemy_pos[1] = 0
     #drawing the rect shape.
     pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    clock.tick(30)
 
     pygame.display.update() #we need to unpdate the screen always 
