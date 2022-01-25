84a460bb9fb0aa62d70cb77d7473869f62e3aae9
sheddyranking <shedyisaac@gmail.com>
2022-01-25 01:11:04 +0100

    draw the first Rect

--- pygame_lesson1.py
+++ pygame_lesson1.py
@@ -18,3 +18,7 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    #drawing the rect shape.
+    pygame.draw.rect(screen, (250,0,0), (400, 300, 50, 50))
+
+    pygame.display.update() #we need to unpdate the screen always 
