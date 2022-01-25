57211aeea78023849260e7268f3166252282cccf
sheddyranking <shedyisaac@gmail.com>
2022-01-25 03:22:00 +0100

    added detection_collision between players and enerny

--- pygame_lesson1.py
+++ pygame_lesson1.py
@@ -17,6 +17,8 @@ BACKGROUND_COLOR = (0,0,0)
 enemy_size = 50
 enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]
 
+SPEED = 10
+
 #creating the display UI
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
@@ -25,6 +27,19 @@ screen = pygame.display.set_mode((WIDTH, HEIGTH))
 game_over = False
 
 clock = pygame.time.Clock()
+
+#detect collision 
+def detect_collision (player_pos, enemy_pos):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[0]
+
+    if(e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_size)):
+        if(e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y + enemy_size)):
+            return True
+    return False
 
 while not game_over:
     for event in pygame.event.get():
@@ -45,12 +60,17 @@ while not game_over:
 
     screen.fill(BACKGROUND_COLOR) #its fills the sides black
 
-    #making the blue rect fall.
+    #making the blue rect fall.(updating pos of enemy)
     if enemy_pos[1] >= 0 and enemy_pos[1] <= HEIGTH:
-        enemy_pos[1] +=10 
+        enemy_pos[1] += SPEED 
     else:
         enemy_pos[0] = random.randint(0, WIDTH-enemy_size) #randomizing the falling pos.
         enemy_pos[1] = 0
+    
+    if detect_collision(player_pos, enemy_pos):
+        game_over = True
+        break
+
     #drawing the rect shape.
     pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
