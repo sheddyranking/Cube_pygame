814b82cec6fb41c85343ae3c89615b44a744b716
sheddyranking <shedyisaac@gmail.com>
2022-01-25 01:39:07 +0100

    tracking the events left and right

--- pygame_lesson1.py
+++ pygame_lesson1.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame #the pygame.org/doc/ for the documentary 
 import sys
 
 #initialling the pygame
@@ -13,7 +13,7 @@ player_size = 50
 #creating the display UI
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 
-#gamne loop
+#game loop
 
 game_over = False
 
@@ -21,6 +21,19 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        
+        #traking the events left and right
+        if event.type == pygame.KEYDOWN:
+
+            x = player_pos[0]
+            y = player_pos[1]
+
+            if event.type == pygame.K_LEFT:
+                x -= 5
+            elif event.type == pygame.K_RIGHT:
+                x += 5
+            player_pos = [x, y] #updating the new position
+
     #drawing the rect shape.
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
 
